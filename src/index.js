5a5f829057abf651489d5d676992bed6514af976
SergeyReznickyy <131711981+SergeyReznickyy@users.noreply.github.com>
2023-11-26 12:44:54 +0200

    ap2

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/your_repo_name">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
